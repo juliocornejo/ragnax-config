f5b5518893ff0d54c60e47d0f7b6b0ba81d9c9ef
juliocornejo <elimino.gente.por.dinero@gmail.com>
2019-12-11 11:50:23 -0300

    first commit

---
+++ src/test/java/com/ragnax/ragnaxconfig/RagnaxConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnax.ragnaxconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagnaxConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
